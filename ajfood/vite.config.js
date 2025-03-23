0d7cb1e97a4cbc123a917c34b92ca2dd7963843f
Robin-Sarwara <sarwararobin2@gmail.com>
2025-03-23 13:24:34 +0530

    Allow Render domain in Vite config

--- ajfood/vite.config.js
+++ ajfood/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-      host: '0.0.0.0',
-      port: process.env.PORT || 5173,
-    }, 
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['ajfoods.onrender.com'], // Add this line
+  },
 })
